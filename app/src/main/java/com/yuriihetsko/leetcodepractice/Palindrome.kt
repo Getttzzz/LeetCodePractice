25e09485cc2da9dc10a3176a12ab1a0e4a3302f1
Yurii Hetsko <getttzzz@gmail.com>
2024-07-02 23:13:31 +0200

    9. Palindrome: Method is done

--- app/src/main/java/com/yuriihetsko/leetcodepractice/Palindrome.kt
+++ app/src/main/java/com/yuriihetsko/leetcodepractice/Palindrome.kt
@@ -4,12 +4,9 @@ package com.yuriihetsko.leetcodepractice
 // 9. Palindrome Number
 // Given an integer x, return true if x is a palindrome, and false otherwise.
 fun main() {
-    println("isPalindrome=${isPalindrome(1234)}")
+    println("isPalindrome=${isPalindrome(123321)}")
 }
 
-// Input could be:
-// 12321
-//
 private fun isPalindrome(x: Int): Boolean {
 //    val result = x % 1000 // Input 12345; Result: 345
 //    val result = x.rem(1000)Input 12345; Result: 345
@@ -19,30 +16,44 @@ private fun isPalindrome(x: Int): Boolean {
 
     var temp = x
     var divisor = 1
-    var quantity = 0
 
     val divisorList = mutableListOf<Int>()
 
     while (temp / divisor != 0) {
         temp -= divisor
-        divisorList.add(divisor)
         divisor *= 10
-        quantity += 1
+
+        divisorList.add(divisor)
 
 //        temp=1233 divisor=10
 //        temp=1223 divisor=100
 //        temp=1123 divisor=1000
 //        temp=123 divisor=10000
-
-        println("temp=$temp divisor=$divisor quantity=$quantity")
     }
 
-    println("$divisorList")
+    divisorList.removeLast() //remove redundant 10_000
 
-    for (i in 0..< quantity) {
-        val firstDigit = x / divisorList[divisorList.size-1]
-        println("firstDigit=$firstDigit x=$x ")
+    var nextX = x
+
+    while (divisorList.isNotEmpty()) {
+        println("nextX(start)=$nextX $divisorList")
+
+        val firstDigit = nextX / divisorList.last()
+        val lastDigit = nextX % divisorList.first()
+        println("firstDigit=$firstDigit lastDigit=$lastDigit")
+
+        if (firstDigit != lastDigit) return false // oh no, x isn't a palindrome
+
+        nextX -= firstDigit * divisorList.last()
+        nextX -= lastDigit
+        nextX /= divisorList.first()
+
+        divisorList.removeLastOrNull()
+        divisorList.removeLastOrNull()
+
+        println("nextX(end)=$nextX \n")
+        // 230 -> 23 = 230/10=23
     }
 
-    return false
+    return true
 }
