07c08068923d279d8c0a27fdf46a5bbd5af3b9e4
Yurii Hetsko <getttzzz@gmail.com>
2024-07-03 21:52:19 +0200

    9. Palindrome: Gotta completely change my prev solution and come up to this

--- app/src/main/java/com/yuriihetsko/leetcodepractice/Palindrome.kt
+++ app/src/main/java/com/yuriihetsko/leetcodepractice/Palindrome.kt
@@ -4,9 +4,10 @@ package com.yuriihetsko.leetcodepractice
 // 9. Palindrome Number
 // Given an integer x, return true if x is a palindrome, and false otherwise.
 fun main() {
-    println("isPalindrome=${isPalindrome(123321)}")
+    println("isPalindrome=${isPalindrome(12321)}")
 }
 
+//First palindromic numbers: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99, 101, 111, 121
 private fun isPalindrome(x: Int): Boolean {
 //    val result = x % 1000 // Input 12345; Result: 345
 //    val result = x.rem(1000)Input 12345; Result: 345
@@ -14,45 +15,25 @@ private fun isPalindrome(x: Int): Boolean {
 //    val result = x.div(1000) // Input: 12345; Result: 12
 //    val result = x / 1000 // Input 12345: Result: 12
 
+    if (x < 0) return false
+    if (x == 0) return true
+
+
     var temp = x
-    var divisor = 1
 
-    val divisorList = mutableListOf<Int>()
+    val digits = mutableListOf<Int>()
 
-    while (temp / divisor != 0) {
-        temp -= divisor
-        divisor *= 10
-
-        divisorList.add(divisor)
-
-//        temp=1233 divisor=10
-//        temp=1223 divisor=100
-//        temp=1123 divisor=1000
-//        temp=123 divisor=10000
+    while (temp > 0) {
+        digits.add(temp % 10)
+        temp /= 10
     }
 
-    divisorList.removeLast() //remove redundant 10_000
+    while (digits.size >= 1) {
+        if (digits.first() != digits.last()) return false // oh no, x isn't a palindrome
 
-    var nextX = x
-
-    while (divisorList.isNotEmpty()) {
-        println("nextX(start)=$nextX $divisorList")
-
-        val firstDigit = nextX / divisorList.last()
-        val lastDigit = nextX % divisorList.first()
-        println("firstDigit=$firstDigit lastDigit=$lastDigit")
-
-        if (firstDigit != lastDigit) return false // oh no, x isn't a palindrome
-
-        nextX -= firstDigit * divisorList.last()
-        nextX -= lastDigit
-        nextX /= divisorList.first()
-
-        divisorList.removeLastOrNull()
-        divisorList.removeLastOrNull()
-
-        println("nextX(end)=$nextX \n")
-        // 230 -> 23 = 230/10=23
+        digits.removeFirstOrNull()
+        digits.removeLastOrNull()
+        println("$digits")
     }
 
     return true
