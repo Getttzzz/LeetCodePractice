0bd08c292fc8fd93082aa2721e53b7fb832a1ba4
Yurii Hetsko <getttzzz@gmail.com>
2024-07-04 00:47:37 +0200

    13. Roman to Integer: Core logic for Roman to Int convertor is created!

--- app/src/main/java/com/yuriihetsko/leetcodepractice/RomanNumberToInt.kt
+++ app/src/main/java/com/yuriihetsko/leetcodepractice/RomanNumberToInt.kt
@@ -1,7 +1,7 @@
 package com.yuriihetsko.leetcodepractice
 
 fun main() {
-
+    println("romanToInt=${romanToInt("LVIII")}")
 }
 
 
@@ -20,7 +20,48 @@ fun main() {
 
 private fun romanToInt(s: String): Int {
 
-    return when (s) {
+    //  Input: s = "III"
+    //  Output: 3
+    //  Explanation: III = 3.
 
+
+    //  Input: s = "LVIII"
+    //  Output: 58
+    //  Explanation: L = 50, V= 5, III = 3.
+
+    // 54 -> LIV (L=50; IV=4)
+    // 58 -> LVIII (L=50; V=5; III=3) //for I I need to check what's next. It could be I or II or III
+    // 59 -> LIX (L=50; IX=9)
+    // 60 -> LX (L=50; X=10)
+    //
+
+    val charList = s.toList()
+    var sum = 0
+
+    charList.forEachIndexed { i, char ->
+        val nextChar = if (i < charList.size-1) charList[i + 1] else null
+
+        println("char=$char nextChar=$nextChar")
+
+        sum += when (char) {
+            'I' -> {
+                when (nextChar) {
+                    null -> 1
+                    'V' -> 4
+                    'X' -> 9
+                    else -> {
+                        if (i < charList.size - 2) 3 else 2
+                    }
+                }
+            }
+            'V' -> 5
+            'X' -> 10
+            'L' -> 50
+            else -> 0
+        }
     }
+
+    println("$sum")
+
+    return sum
 }
