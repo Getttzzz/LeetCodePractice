2f582d321804787e4f18b4fc28d04dcd0b554011
Yurii Hetsko <getttzzz@gmail.com>
2024-07-04 12:38:28 +0200

    53. Maximum Subarray (Medium): start

---
+++ app/src/main/java/com/yuriihetsko/leetcodepractice/MaximumSubArray.kt
@@ -0,0 +1,20 @@
+package com.yuriihetsko.leetcodepractice
+
+fun main() {
+
+}
+
+//Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+//Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+//Output: 6
+//Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+//Input: nums = [5,4,-1,7,8]
+//Output: 23
+//Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+private fun maxSubArray(nums: IntArray): Int {
+
+
+    return 1
+}
